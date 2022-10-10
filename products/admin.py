e210b0bf042e881c6dcf25bda3345a7d35665d89
renatabiniek <renata.biniek@gmail.com>
2022-10-10 10:59:36 +0000

    Customize display in admin panel for products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin - display category model fields"""
+    list_display = ('friendly_name', 'name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Admin - display product model fields"""
+    list_display = ('name', 'sku', 'category', 'price','is_available', 'image')
+    list_filter = ('category', 'is_available')
+    search_fields = ['name', 'description']
+
+    ordering = ('name',)
