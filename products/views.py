20a7aa96dbee8d393b3d5fc0d266ad639d4d782c
renatabiniek <renata.biniek@gmail.com>
2022-10-11 21:11:55 +0000

    Add category filter and category badges

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 """Views for Product app"""
 
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,18 +11,31 @@ def show_all_products(request):
     Show all products to the superuser,
     and only available ones to other users.
     """
-
+    
     if request.user.is_superuser:
         products = Product.objects.all()
     else:
         products = Product.objects.filter(is_available=True)
 
+    categories = None
+    cats = Category.objects.all()
+
+    if request.GET:
+        # Filter by category
+        if 'category' in request.GET:
+            # split it into a list at the commas
+            categories = request.GET['category'].split(',')
+            # use the list to filter the current query set
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
     context = {
         'products': products,
+        'current_categories': categories,
+        'cats': cats,
     }
-   
-    return render(request, 'products/products.html', context)
 
+    return render(request, 'products/products.html', context)
 
 def show_product_detail(request, product_id):
     """Show details of individual products."""
