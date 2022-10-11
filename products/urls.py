e8c0d78e6d456b9d83207fffe0ff0b24d802deeb
renatabiniek <renata.biniek@gmail.com>
2022-10-11 15:30:19 +0000

    Set up view, urls and template for product details page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_all_products, name='products'),
+    path('<int:product_id>/', views.show_product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+"""Views for Product app"""
+
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -18,5 +20,17 @@ def show_all_products(request):
     context = {
         'products': products,
     }
-    
+   
     return render(request, 'products/products.html', context)
+
+
+def show_product_detail(request, product_id):
+    """Show details of individual products."""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
