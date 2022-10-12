4f555c7ffd092e8703726f4f794a400897174119
renatabiniek <renata.biniek@gmail.com>
2022-10-12 21:27:16 +0000

    Add search functionality to shop page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 """Views for Product app"""
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product, Category
 
 # Create your views here.
@@ -17,8 +19,9 @@ def show_all_products(request):
     else:
         products = Product.objects.filter(is_available=True)
 
-    categories = None
     cats = Category.objects.all()
+    categories = None
+    search_term = None
 
     if request.GET:
         # Filter by category
@@ -28,11 +31,26 @@ def show_all_products(request):
             # use the list to filter the current query set
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+        # Search
+        if 'q' in request.GET:
+            search_term = request.GET['q']
+            # if the query is blank
+            if not search_term:
+                messages.error(
+                    request, "You didn't enter anything in the search field.")
+                return redirect(reverse('products'))
+
+            # if the query is not blank; returning results where the search term matched in either product name or description with case insensitive queries
+            matched_terms = Q(name__icontains=search_term) | Q(
+                description__icontains=search_term)
+            products = products.filter(matched_terms)
+
 
     context = {
         'products': products,
         'current_categories': categories,
         'cats': cats,
+        'search_term': search_term,
     }
 
     return render(request, 'products/products.html', context)
