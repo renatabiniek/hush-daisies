fa9ae68619c23600f88521ad79d479ced4247092
renatabiniek <renata.biniek@gmail.com>
2022-10-13 21:13:51 +0000

    Add sorting functionality to show_all_products view

--- products/views.py
+++ products/views.py
@@ -22,6 +22,8 @@ def show_all_products(request):
     cats = Category.objects.all()
     categories = None
     search_term = None
+    sort = None
+    direction = None
 
     if request.GET:
         # Filter by category
@@ -39,18 +41,37 @@ def show_all_products(request):
                 messages.error(
                     request, "You didn't enter anything in the search field.")
                 return redirect(reverse('products'))
-
             # if the query is not blank; returning results where the search term matched in either product name or description with case insensitive queries
             matched_terms = Q(name__icontains=search_term) | Q(
                 description__icontains=search_term)
             products = products.filter(matched_terms)
+        # Sorting
+        if 'sort' in request.GET:
+            # Create the sortkey variable
+            sortkey = request.GET['sort']
+            sort = sortkey
+            # Allow case insensitive sorting by product name by adding temporary field on the Product model
+            if sortkey == "name":
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
 
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                # Reverse the sorting order if direction descending
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            # Use model method to sort products
+            products = products.order_by(sortkey)
+
+    # Return the sorting methodology to the template
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'current_categories': categories,
         'cats': cats,
         'search_term': search_term,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
