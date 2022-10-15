8ddfab0e458b283c2ae5b5686a31b964aa28adf6
renatabiniek <renata.biniek@gmail.com>
2022-10-15 14:16:09 +0000

    Add basket content to context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,6 +1,8 @@
 """Context processor for basket content"""
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
     """
@@ -12,6 +14,18 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    # get the basket from the session 
+    basket = request.session.get('basket', {})
+    # iterate through items in the basket from the session
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
