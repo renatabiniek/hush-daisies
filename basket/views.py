b73791043564f5b0e8e89601135cbb1b53938df9
renatabiniek <renata.biniek@gmail.com>
2022-10-16 16:17:44 +0000

    Add functionality to update qty in the basket

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,8 @@
 """Views for Basket app"""
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 # Create your views here.
+
 
 def view_basket(request):
     """Show items in the basket"""
@@ -29,3 +30,26 @@ def add_to_basket(request, item_id):
     
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """
+    Adjust basket content.
+    Redirect always to the shopping basket page.
+    If qty > 0, set new qty, otherwise remove item.
+    Override session with new basket content.
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    print(quantity)
+
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+    
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
