f2cc4d133f12ce9abc3bfacf94ac4755a311f596
renatabiniek <renata.biniek@gmail.com>
2022-10-24 21:06:50 +0000

    Update profile views with user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
 """Views for Profiles app"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 # Create your views here.
 
 
 def profile(request):
     """Display user profile page"""
+    user_profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'user_profile': user_profile,
+    }
 
     return render(request, template, context)
