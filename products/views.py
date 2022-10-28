9c8b83569544d89e081a4294bc531803d15cf97c
renatabiniek <renata.biniek@gmail.com>
2022-10-28 21:37:28 +0000

    Add delete product functionality and change redirect in add_product

--- products/views.py
+++ products/views.py
@@ -99,9 +99,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product was added to the store!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 
@@ -150,3 +150,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """View for admin to delete product from the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
