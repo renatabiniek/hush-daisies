2e8f12274e52a21d5e9e953cc5da4fe214ee42c5
renatabiniek <renata.biniek@gmail.com>
2022-10-28 19:25:27 +0000

    Create product form for admins

---
+++ products/forms.py
@@ -0,0 +1,27 @@
+"""Product forms"""
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Form for superuser to add products to the store in the frontend
+    """
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+   
+    def __init__(self, *args, **kwargs):
+        """
+        Override init to make changes to the fields.
+        Create list of tuples of the friendly names and category ids,
+        to use as choices on the form
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.field['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-teal'
