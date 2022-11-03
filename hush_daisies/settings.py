38b39a598d41d89c0e1992e773107bf31cda776e
renatabiniek <renata.biniek@gmail.com>
2022-11-03 23:35:25 +0000

    Set up sending real emails

--- hush_daisies/settings.py
+++ hush_daisies/settings.py
@@ -226,4 +226,15 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Email
-DEFAULT_FROM_EMAIL = 'hushdaisies@example.com'
+if 'DEVELOPMENT' in os.environ:
+    # in development emails logged to console
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'hushdaisies@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
