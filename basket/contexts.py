e6d04332c4fb06fd32455a5c9f386796f734ccea
renatabiniek <renata.biniek@gmail.com>
2022-11-04 19:24:47 +0000

    Adjust navigation bar for smaller screens

--- basket/contexts.py
+++ basket/contexts.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def basket_contents(request):
     """
     Context processor that returns context dictionary
